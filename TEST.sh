bcc109d8c204b32ef70b50ff0823681ba4836f0c
Уйманов Юрий <urauymanov@yandex.ru>
2020-10-26 14:26:49 +0700

    Issue #1 пункт 10. Создан скрипт для тестирования приложения.

---
+++ TEST.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Scenario 1
+echo the quick brown fox jumps over the lazy dog
+echo the quick brown fox jumps over the lazy dog | java -jar MyKotlinApp.jar
+
+echo Scenario 2
+echo the lazy programmer sleep under the table
+echo the lazy programmer sleep under the table | java -jar MyKotlinApp.jar
+
+echo Scenario 3
+echo Lorem ipsum dolor sit amet
+echo Lorem ipsum dolor sit amet | java -jar MyKotlinApp.jar
+
+echo Scenaro 4
+echo  
+echo  |java -jar MyKotlinApp.jar
+
+echo "Scenarios done!"
+
